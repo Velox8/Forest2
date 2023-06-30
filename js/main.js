eb18ae371b7a73165cc1d7e7de0ee844a97fc788
Max <syst38@gmail.com>
2023-06-30 02:16:59 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -3,45 +3,41 @@ document.addEventListener('DOMContentLoaded', function () {
 	const navBar = document.querySelector('.nav-mobile');
 	const footerYear = document.querySelector('.footer-year');
 	const navItem = document.querySelectorAll('.a');
-	const menuItems = document.querySelectorAll('.nav-item')
-	const scrollSpySections = document.querySelectorAll('.section')
-
+	const menuItems = document.querySelectorAll('.nav-item');
+	const scrollSpySections = document.querySelectorAll('.section');
+	const faSolid = document.querySelector('.fa-tree');
 
 	const handleScrollSpy = () => {
-		
-	
-			const sections = []
-	
-			scrollSpySections.forEach(section => {
-			
+		const sections = [];
 
-				
-	
-				if(window.scrollY <= section.offsetTop + section.offsetHeight -100) {
-					sections.push(section)
-				   
-					const activeSection = document.querySelector(`[href*="${sections[0].id}"]`)
-					
-					menuItems.forEach(item => item.classList.remove('active'))
-	
-					activeSection.classList.add('active')
-				}
-	
-				if ( window.innerHeight + window.scrollY >= document.body.offsetHeight - 200) {
-					const lastSection = document.querySelector('a:last-of-type')
-	
-					menuItems.forEach(item => item.classList.remove('active'))
-	
-					lastSection.classList.add('active')
-				}
-	
-			})
-		
-	}
-	
-	
-	window.addEventListener('scroll', handleScrollSpy)
-	
+		scrollSpySections.forEach((section) => {
+			if (window.scrollY <= section.offsetTop + section.offsetHeight - 100) {
+				sections.push(section);
+
+				const activeSection = document.querySelector(
+					`[href*="${sections[0].id}"]`
+				);
+
+				menuItems.forEach((item) => item.classList.remove('active'));
+
+				activeSection.classList.add('active');
+			}
+
+			if (
+				window.innerHeight + window.scrollY >=
+				document.body.offsetHeight - 200
+			) {
+				const lastSection = document.querySelector('a:last-of-type');
+
+				menuItems.forEach((item) => item.classList.remove('active'));
+
+				lastSection.classList.add('active');
+			}
+		});
+	};
+
+
+	window.addEventListener('scroll', handleScrollSpy);
 
 	const addActive = () => {
 		navBar.classList.toggle('nav-mobile-active');
@@ -57,6 +53,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
 	handleCurrentYear();
 
+	faSolid.addEventListener("click", function() {
+		window.location.href = "index.html#home";
+	
+	});
 	burgerBtn.addEventListener('click', addActive);
 	navItem.forEach((item) => {
 		item.addEventListener('click', removeA);
